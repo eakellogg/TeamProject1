fc26f16dfd98f4c1806d2a726e6b541ff765c0ae
njdj1013 <nathanjdejong@gmail.com>
2014-02-04 22:43:46 -0600

    Update Engine.h

--- Engine.h
+++ Engine.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include <iostream>
 #include <tuple>
@@ -10,7 +12,7 @@ using namespace std;
 
 
 class Engine {
-	vector<Table> tables;
+	vector<Table*> tables;
 
 public:
 /*********************************************************************************
@@ -29,46 +31,67 @@ public:
 
 
 	//we can remove this section if we find we don't need it
+	vector<Table*> getTables() { return tables; }
 
 
 /*********************************************************************************
 	modifying functions
 *********************************************************************************/
 
-
-	void createTable(string tableName, vector<string> types, vector<string> columnTitles) {
-		tables.push_back(Table(tableName, types, columnTitles));
+	// create a table with the row types and column titles given
+	void createTable(string tableName, vector<string> rowTypes, vector<string> columnTitles) {
+		tables.push_back(new Table(tableName, rowTypes, columnTitles));
 	}
 
 
-	void deleteTable(string tableName) {
+	// delete a table from the database
+	void dropTable(string tableName) {
 		// search database for a table with matching name; delete table if it exists
 		for (int i = 0; i < tables.size(); i++) {
-			if (tables[i].getTableName() == tableName) {
-				tables.erase[i];
-				break;
+			if (tables[i]->getTableName() == tableName) {
+				tables.erase(tables.begin() + i); // must use tables.begin() to get iterator; increment with i
+				return;
 			}
 		}
+		throw "table does not exist";
 	}
 
 
+	// insert a row into the table with the proper name
 	void insertInto(string tableName, vector<string> row, vector<string> rowTypes) {
-		// search database for a table with matching name; add row to correct table if it exists
+		Table* tempTable = findTable(tableName);
+		tempTable->addRow(row, rowTypes);
+	}
+
+
+	// update a row in the table with the proper name
+	void update(string tableName, vector<string> row, vector<string> rowTypes) {
+		int pos = 0; //!!!!!!!!!!!!!! REALLY NEED TO FIND THIS METHOD
+		Table* tempTable = findTable(tableName);
+		tempTable->updateRow(row, rowTypes, pos);		
+	}
+
+
+	// delete a row in the table with the proper name
+	void deleteFrom(string tableName, int pos) {
+		Table* tempTable = findTable(tableName);
+		tempTable->deleteRow(pos);
+	}
+
+
+/*********************************************************************************
+	helper functions
+*********************************************************************************/
+
+
+	// search database for a table with matching name
+	Table* findTable(string tableName) {
+		// compare passed name to names of all tables in the database
 		for (int i = 0; i < tables.size(); i++) {
-			if (tables[i].getTableName() == tableName) {
-				tables[i].addRow(row, rowTypes);
-				break;
+			if (tables[i]->getTableName() == tableName) {
+				return tables[i];
 			}
 		}
-	}
-
-
-	void update() {
-
-	}
-
-
-	void deleteFrom() {
-
+		throw "table does not exist"; // throwing exception prevents needing checks for unfound tables in other functions
 	}
 };
