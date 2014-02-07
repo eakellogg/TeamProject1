cf5a90f5ce4a4c3e04d402e6df183860bd20a114
njdj1013 <nathanjdejong@gmail.com>
2014-02-07 13:41:58 -0600

    Update Attribute.h

--- DBMS/DBMS/Attribute.h
+++ DBMS/DBMS/Attribute.h
@@ -1,9 +1,40 @@
+#pragma once
+
 #include <string>
+#include <iostream>
+
+using namespace std;
 // extended by varchar, int
-class Attribute
-{
-	std::string type;
-	std::string value;
+class Attribute {
+	string type;
+	string value;
+
+public:
+	/*********************************************************************************
+	constructor
+	*********************************************************************************/
 
 
+	Attribute(string type, string value)
+		:type(type), value(value)
+	{	}
+
+
+	/*********************************************************************************
+	non-modifying functions / accessor methods
+	*********************************************************************************/
+
+
+	string getType() { return type; }
+
+	string getValue() { return value; }
+
+
+	/*********************************************************************************
+	modifying functions
+	*********************************************************************************/
+
+	void setValue(string newValue) {
+		type = newValue;
+	}
 };
