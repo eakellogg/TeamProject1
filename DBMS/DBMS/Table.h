a9c7cd4fe4e3524be8fcc0e7d80de605440b69a6
njdj1013 <nathanjdejong@gmail.com>
2014-02-07 13:41:14 -0600

    Update Table.h

--- DBMS/DBMS/Table.h
+++ DBMS/DBMS/Table.h
@@ -5,111 +5,131 @@
 #include <tuple>
 #include <vector>
 #include <typeinfo>
+#include <map>
+
+#include "Attribute.h"
 
 using namespace std;
 
 
 class Table {
 	string tableName;
-	vector< vector<string> > data;
-	vector<string> types;
+	map< string, vector<Attribute*> > data; // the rows of the table
+	vector<string> columnTypes;
 	vector<string> columnTitles;
-
+	vector<Attribute*> currentRow;
 public:
-/*********************************************************************************
+	/*********************************************************************************
 	constructor
-*********************************************************************************/
-	
+	*********************************************************************************/
 
-	Table(string tableName, vector<string> types, vector<string> columnTitles)
-		:tableName(tableName), types(types), columnTitles(columnTitles)
+
+	Table(string tableName, vector<string> columnTypes, vector<string> columnTitles)
+		:tableName(tableName), columnTypes(columnTypes), columnTitles(columnTitles)
 	{	}
 
 
-/*********************************************************************************
+	/*********************************************************************************
 	non-modifying functions / accessor methods
-*********************************************************************************/
+	*********************************************************************************/
 
 
 	string getTableName() { return tableName; }
 
-	vector< vector<string> > getData() { return data; }
-	
-	
-/*********************************************************************************
+	map< string, vector<Attribute*> > getData() { return data; }
+
+	vector<string> getColumnTypes() { return columnTypes; }
+
+	vector<string> getColumnTitles() { return columnTitles; }
+
+	vector<Attribute*> getCurrentRow() { return currentRow; };
+
+
+	/*********************************************************************************
 	modifying functions
-*********************************************************************************/
+	*********************************************************************************/
 
 
 	// add a row to the table
-	void addRow(vector<string> row, vector<string> rowTypes) {
+	void addRow(string key, vector<Attribute*> row, vector<string> rowTypes) {
 		if (checkMatchingTypes(rowTypes)) {
-			data.push_back(row);
+			data.insert(make_pair(key, row));
 			return;
 		}
 		throw "types do not match";
 	}
 
-
+	/*
 	// update an already existing row
 	void updateRow(vector<string> newRow, vector<string> rowTypes, int pos) { // need diff. than pos
-		if (checkMatchingTypes(rowTypes)) {
-			data[pos] = newRow;
-			return;
-		}
-		throw "types do not match";
+	if (checkMatchingTypes(rowTypes)) {
+	data[pos] = newRow;
+	return;
 	}
-
+	throw "types do not match";
+	}
+	*/
 
 	// delete a row from the table
-	void deleteRow(int pos) {
-		if (pos < data.size()) {
-			data.erase(data.begin() + pos); // must use data.begin() to get iterator; increment with pos
-			return;
+	void deleteRow(string key) {
+		try {
+			data.erase(key);
 		}
-		throw "row does not exist - improper index";
+		catch (out_of_range) {
+			throw "key does not exist";
+		}
 	}
 
 
 	// find and return a specific row in the table
-	vector<string> getRow(int pos) {
-		if (pos < data.size()) {
-			return data[pos];
+	vector<Attribute*> getRow(string key) {
+		try {
+			return data.at(key);
 		}
-		throw "row does not exist - improper index";
+		catch (out_of_range) {
+			throw "key does not exist";
+		}
 	}
 
 
 	// find and return a specific item in a specific row in the table
-	string getItem(string columnName, int pos) {
-		if (pos >= data.size()) {
-			throw "row does not exist - improper index";
-		}
-		vector<string> tempRow = data[pos];
+	Attribute* getItem(string key, string columnName) {
+		vector<Attribute*> currentRow = data.at(key);
 
 		// search the temporary row for correct item based on column name
 		for (int i = 0; i < columnTitles.size(); i++) {
 			if (columnName == columnTitles[i]) {
-				return tempRow[i];
+				return currentRow[i];
 			}
 		}
 		throw "column does not exist";
 	}
 
 
-/*********************************************************************************
+	/*********************************************************************************
 	helper functions
-*********************************************************************************/
+	*********************************************************************************/
 
 
 	// check to make sure the types of the table's rows and the passed types are equivalent
 	bool checkMatchingTypes(vector<string> rowTypes) {
 		// check every type of the row in question and compare with table's row types
-		for (int i = 0; i < types.size(); i++) {
-			if (rowTypes[i] != types[i]) {
+		for (int i = 0; i < columnTypes.size(); i++) {
+			if (rowTypes[i] != columnTypes[i]) {
 				return false; // incorrect type was found in the process of adding
 			}
 		}
 		return true;
+	}
+
+
+	// takes in a variable name, searches the current row, and returns the value of the variable name
+	Attribute* getVariable(string variableName) {
+		for (int i = 0; i < columnTitles.size(); i++) {
+			if (columnTitles[i] == variableName) {
+				return currentRow[i];
+			}
+		}
+		throw "variable does not exist";
 	}
 };
