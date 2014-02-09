09703d682e3c86bb705a9a9bdb660cdb6505e9a7
njdj1013 <nathanjdejong@gmail.com>
2014-02-09 16:07:50 -0600

    Update Application.cpp

--- DBMS/Main/Application.cpp
+++ DBMS/Main/Application.cpp
@@ -4,10 +4,11 @@
 int main()
 {
 	try {
-		
+		// create information to put into table to prepare testing
 		Attribute* namea = new Attribute("string", "Zach Brown");
 		Attribute* nameb = new Attribute("string", "Jacob Zerr");
 		Attribute* namec = new Attribute("string", "Victoria Elliott");
+		Attribute* named = new Attribute("string", "Johnny Football");
 
 		Attribute* schoola = new Attribute("string", "Texas A&M");
 		Attribute* schoolb = new Attribute("string", "Kansas University");
@@ -18,35 +19,53 @@ int main()
 		Attribute* agec = new Attribute("int", "18");
 
 		Attribute* citya = new Attribute("string", "College Station");
-		Attribute* statea = new Attribute("string", "Texas");
+		Attribute* cityb = new Attribute("string", "San Antonio");
+		Attribute* cityc = new Attribute("string", "Frederick");
 
-		vector<string> columnTypes = vector<string>{"string", "string", "int"};
-		vector<string> badRowTypes = vector<string>{"string", "string", "string"};
-		vector<string> columnTitles = vector<string>{"name", "school", "age"};
+		Attribute* statea = new Attribute("string", "Texas");
+		Attribute* stateb = new Attribute("string", "Maryland");
+		Attribute* statec = new Attribute("string", "Missouri");
+
+		vector<string> columnTypesA = vector<string>{"string", "string", "int"};
+		vector<string> columnTypesB = vector<string>{"string", "string", "string"};
+		vector<string> badColumnTypes = vector<string>{"int", "int", "int"};
+
+		vector<string> columnTitlesA = vector<string>{"name", "school", "age"};
+		vector<string> columnTitlesB = vector<string>{"name", "city", "state"};
 
 		vector<Attribute*> student1 = vector<Attribute*>{namea, schoola, agea};
 		vector<Attribute*> student2 = vector<Attribute*>{nameb, schoolb, ageb};
-		vector<Attribute*> student3 = vector<Attribute*>{namea, schoola, agec};
+		vector<Attribute*> student3 = vector<Attribute*>{namec, schoolc, agec};
 
-		vector<Attribute*> student4 = vector<Attribute*>{namea, schoola, statea};
-		
-		Engine engine = Engine();
+		vector<Attribute*> person1 = vector<Attribute*>{namea, citya, statea};
+		vector<Attribute*> person2 = vector<Attribute*>{nameb, cityb, stateb};
+		vector<Attribute*> person3 = vector<Attribute*>{named, cityc, statec};
 
-		vector<string> columnTypesA = vector<string>{"string", "string", "string"};
-		vector<string> columnTitlesA = vector<string>{"name", "school", "state"};
-		vector<string> columnTypesB = vector<string>{"string", "string", "int"};
-		vector<string> columnTitlesB = vector<string>{"name", "school", "age"};
+		Engine testEngine = Engine();
 
-		Table* tbla = new Table("TableA", "state", columnTypesA, columnTitlesA);
-		tbla->addRow(student4, columnTypesA);
-		Table* tblb = new Table("testTableB", "age", columnTypesB, columnTitlesB);
-		tblb->addRow(student3, columnTypesB);
-		tblb->addRow(student1, columnTypesB);
+		testEngine.createTable("students", "name", columnTypesA, columnTitlesA);
+		testEngine.createTable("people", "state", columnTypesB, columnTitlesB);
+
+		testEngine.insertInto("students", student1, columnTypesA);
+		testEngine.insertInto("students", student2, columnTypesA);
+		testEngine.insertInto("students", student3, columnTypesA);
+
+		testEngine.insertInto("people", person1, columnTypesB);
+		testEngine.insertInto("people", person2, columnTypesB);
+		testEngine.insertInto("people", person3, columnTypesB);
+
+		Table* tbla = testEngine.getTables()[0];
+		Table* tblb = testEngine.getTables()[1];
+
+		tbla->printTable();
+		tblb->printTable();
+
 		Table* tblc = NULL;
 		Table* tbld = NULL;
 
-		tblc = engine.naturalJoin(tblb, tbla);
-		tbld = engine.crossProduct(tblc, tbla);
+		tblc = testEngine.naturalJoin(tbla, tblb);
+		tblc->printTable();
+		tbld = testEngine.naturalJoin(tblc, tbla);
 		
 		tbld->printTable();
 		
