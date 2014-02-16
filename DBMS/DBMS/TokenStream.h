78184d140209b1eef53c1354b94611b258e343a3
njdj1013 <nathanjdejong@gmail.com>
2014-02-16 00:08:10 -0600

    Update TokenStream.h

--- DBMS/DBMS/TokenStream.h
+++ DBMS/DBMS/TokenStream.h
@@ -1,36 +1,51 @@
-
 #ifndef TOKEN_STREAM_H
 #define TOKEN_STREAM_H
 
 #include <iostream>
 using namespace std;
 const static string FILLER = "FILLER";
-class Token{
+
+
+class Token {
 public:
+	// return value of the token
 	string getValue(){
 		return value;
 	}
+	
+	// trtuen type of the token
 	string getType(){
 		return type;
 	}
+	
+	// constructor for Token
 	Token( string type, string value) : value(value), type(type){}
+	
 private:
 	string value;
 	string type;
 
 };
-class TokenStream{
 
+class TokenStream {
 public:
+	// get a token from the token stream
 	Token getToken(){
 		if (tokens.size() != 0){
 			Token t = tokens[tokens.size() - 1]; tokens.pop_back();  return t;
 		}
 		else return Token(FILLER, "NOTHING HERE");
-				}
-	void  pushToken(Token t){ tokens.push_back(t); }
+	}
+	
+	// push a token into the token stream
+	void pushToken(Token t){ tokens.push_back(t); }
+	
+	// constructor for TokenStream
 	TokenStream(){ index = 0; }
-	int  getCount() { return tokens.size(); }
+	
+	// get the number of token currently in the stream
+	int getCount() { return tokens.size(); }
+	
 private:
 	vector<Token> tokens;
 	int index;
