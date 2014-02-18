034a0eb72f167ba6c801cff1aadd1ecc9829ae9f
eakellogg <bookworm920@nat-10-200-54-122.tamulink.tamu.edu>
2014-02-17 21:55:30 -0600

    Merge branch 'master' of https://github.com/eakellogg/TeamProject1
    Conflicts:
    DBMS/DBMS/lexer.h

--- DBMS/DBMS/lexer.h
+++ DBMS/DBMS/lexer.h
@@ -1,51 +1,164 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 #ifndef LEXER_H
 #define LEXER_H
 
 #include <string>
-
 using namespace std;
+const static string DB_COMMAND = "DB_COMMAND";
 
-
-const static string select = “select”;
- const static string project = “project”;
- const static string rename = “rename”;
- const static string join = “JOIN”;
- const static string union = “+”;
- const static string difference = "-";
- const static string product = “*”;
+// of type db_command :
+=======
+ #ifndef LEXER_H
+ #define LEXER_H
  
+const static string DB_COMMAND = "DB_COMMAND";
+
  // of type db_command :
- const static string close = “CLOSE”;
- const static string create_table = “CREATE TABLE”;
- const static string delete = “DELETE FROM”;
- const static string exit = “EXIT”;
- const static string insert = “INSERT INTO”;
- const static string open = “OPEN”;
- const static string primary_key = “PRIMARY KEY”
- const static string set = “SET”;
- const static string show = “SHOW”;
- const static string update = “UPDATE”;
- const static string values_from = “VALUES FROM”;
- const static string values_from_relation = “VALUES FROM RELATION”;
- const static string where = “WHERE”;
- const static string write = “WRITE”;
-  
- //of type operator : 
- const static string equals = “==”;
- const static string not_equal = “!=”;
- const static string less_than = “<”;
- const static string less_equal = “<=”;
- const static string arrow = "<-";
- const static string greater_than = “>”;
- const static string greater_equal = “>=”;
- const static string or = “||”;
- const static string and = “&&”;
+>>>>>>> bba805f00c659f73faf70a3dd5311c333f19e447
+const static string CLOSE = "CLOSE";
+const static string CREATE_TABLE = "CREATE TABLE";
+const static string DELETED = "DELETE FROM";
+const static string EXIT = "EXIT";
+const static string INSERT = "INSERT INTO";
+const static string OPEN = "OPEN";
+const static string PRIMARY_KEY = "PRIMARY KEY";
+const static string SET = "SET";
+const static string SHOW = "SHOW";
+const static string UPDATE = "UPDATE";
+const static string VALUES_FROM = "VALUES FROM";
+const static string VALUES_FROM_RELATION = "VALUES FROM RELATION";
+const static string WHERE = "WHERE";
+const static string WRITE = "WRITE";
+<<<<<<< HEAD
+
+//of type query_command
+const static string SELECT = "select";
+const static string PROJECT = "project";
+const static string RENAME = "rename";
+const static string JOIN = "JOIN";
+const static string UNION = "+";
+const static string DIFFERENCE = "-";
+const static string PRODUCT = "*";
+
+const static string OPERATOR_TYPE = "OPERATOR";
+//of type operator :
+const static string ASSIGNMENT = "=";
+const static string EQUALS = "==";
+const static string NOT_EQUAL = "!=";
+const static string LESS_THAN = "<";
+const static string LESS_EQUAL = "<=";
+const static string ARROW = "<-";
+const static string GREATER_THAN = ">";
+const static string GREATER_EQUAL = ">=";
+//const static string OR = "||";
+//const static string AND = "&&";
+
+const static string IDENTIFIER = "IDENTIFIER";
+
+const static string TYPE = "TYPE";
+const static string OPEN_PAREN = "(";
+const static string CLOSE_PAREN = ")";
+const static string COMMA = ",";
+const static string SETT = "SET";
+const static string STRING_LITERAL = "STRING_LITERAL";
+const static string INT_LITERAL = "STRING_INT";
+
+//other types that Ill type up later
 /* other token types:
  * type			      ie. varchar(20)  or int
  * string_literal
  * int_literal
  * identifier
  * symbol		      can be open_paren, close_paren, or comma
-*/
-
+ */
 #endif
+=======
+
+=======
+ #ifndef LEXER_H
+ #define LEXER_H
+ 
+const static string DB_COMMAND = "DB_COMMAND";
+
+ // of type db_command :
+const static string CLOSE = "CLOSE";
+const static string CREATE_TABLE = "CREATE TABLE";
+const static string DELETED = "DELETE FROM";
+const static string EXIT = "EXIT";
+const static string INSERT = "INSERT INTO";
+const static string OPEN = "OPEN";
+const static string PRIMARY_KEY = "PRIMARY KEY";
+const static string SET = "SET";
+const static string SHOW = "SHOW";
+const static string UPDATE = "UPDATE";
+const static string VALUES_FROM = "VALUES FROM";
+const static string VALUES_FROM_RELATION = "VALUES FROM RELATION";
+const static string WHERE = "WHERE";
+const static string WRITE = "WRITE";
+
+>>>>>>> bba805f00c659f73faf70a3dd5311c333f19e447
+=======
+ #ifndef LEXER_H
+ #define LEXER_H
+ 
+const static string DB_COMMAND = "DB_COMMAND";
+
+ // of type db_command :
+const static string CLOSE = "CLOSE";
+const static string CREATE_TABLE = "CREATE TABLE";
+const static string DELETED = "DELETE FROM";
+const static string EXIT = "EXIT";
+const static string INSERT = "INSERT INTO";
+const static string OPEN = "OPEN";
+const static string PRIMARY_KEY = "PRIMARY KEY";
+const static string SET = "SET";
+const static string SHOW = "SHOW";
+const static string UPDATE = "UPDATE";
+const static string VALUES_FROM = "VALUES FROM";
+const static string VALUES_FROM_RELATION = "VALUES FROM RELATION";
+const static string WHERE = "WHERE";
+const static string WRITE = "WRITE";
+
+>>>>>>> bba805f00c659f73faf70a3dd5311c333f19e447
+//of type query_command
+const static string SELECT = "select";
+const static string PROJECT = "project";
+ const static string RENAME = “rename”;
+ const static string JOIN = “JOIN”;
+ const static string UNION = “+”;
+const static string DIFFERENCE = "-";
+ const static string PRODUCT = “*”;
+
+ const static string OPERATOR_TYPE = "OPERATOR";
+ //of type operator : 
+const static string EQUALS = "==";
+const static string NOT_EQUAL = "!=";
+const static string LESS_THAN = "<";
+const static string LESS_EQUAL = "<=";
+const static string ARROW = "<-";
+const static string GREATER_THAN = ">";
+const static string GREATER_EQUAL = ">=";
+const static string OR = "||";
+const static string AND = "&&";
+
+ const static string IDENTIFIER = "IDENTIFIER";
+ 
+ //other types that Ill type up later
+  /* other token types:
+  * type			      ie. varchar(20)  or int
+  * string_literal
+  * int_literal
+  * identifier
+  * symbol		      can be open_paren, close_paren, or comma
+ */
+ 
+ #endif
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> bba805f00c659f73faf70a3dd5311c333f19e447
+=======
+>>>>>>> bba805f00c659f73faf70a3dd5311c333f19e447
+=======
+>>>>>>> bba805f00c659f73faf70a3dd5311c333f19e447
